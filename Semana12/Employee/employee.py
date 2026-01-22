87888c01f322e3d9bf0c76a1a9b42a156f7c2839
MiguelOviedoRojas <moviedo20@gmail.com>
2026-01-21 23:09:45 -0600

    Added exercise 3 and ExtraExercise 1

---
+++ Semana12/Employee/employee.py
@@ -0,0 +1,47 @@
+class Employee():
+    def __init__(self, name, salary):
+        self.__name = name
+        self.__salary = salary
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    @property
+    def user_salary(self):
+        return self.__salary
+    
+    @user_salary.setter
+    def user_salary(self, salary):
+        if isinstance(salary,(int, float)):
+            if salary < 0:
+                raise ValueError
+            else:
+                self.__salary = salary
+        else:
+            raise TypeError
+        
+
+
+    def promote(self, percentage):
+        new_salary = (self.user_salary * percentage) + self.user_salary
+        self.user_salary = new_salary
+        return self.user_salary
+    
+    def print_salary(self):
+        return self.user_salary
+        
+
+
+
+
+employee_name = "Migue"
+employee_salary = float(input("Insert Salary: "))
+employee = Employee(employee_name, employee_salary)
+print(f"Employee Name: {employee.name}, Employee Salary: {employee.user_salary}.")
+try:
+    employee.salary = employee_salary
+except ValueError as ex:
+    print(ex)
+salary_promote = float(input("Insert Promote Salary: "))
+print(f"New Salary is: {employee.promote(salary_promote)}")

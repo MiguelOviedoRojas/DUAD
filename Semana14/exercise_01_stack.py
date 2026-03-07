fba1e731bbb8034bc1f25f2fdfffa2afb0c9e325
MiguelOviedoRojas <moviedo20@gmail.com>
2026-03-06 18:19:56 -0600

    Added Fixes in exercise 01 and 02

--- Semana14/exercise_01_stack.py
+++ Semana14/exercise_01_stack.py
@@ -10,55 +10,42 @@ class Node:
 class Stack:
     head: Node
 
-    def __init__(self, head):
-        self.head = head
+    def __init__(self):
+        self.head = None
 
     def print_structure(self):
-        if self.head.next is not None:
+        if self.head is not None:
             current_node = self.head
 
             while current_node is not None:
                 print(current_node.data)
                 current_node = current_node.next
         else:
-            print("No hay elementos en el Stack")
+            print("Stack doesn't have Elements")
         
-    
     def push(self, new_node):
+        if self.head is None:
+            self.head = new_node
+        else:
             new_node.next = self.head
             self.head = new_node
 
     def pop(self):
-        if self.head.next is not None:
+        if self.head is not None:
             self.head = self.head.next
+        else:
+            print("Stack doesn't have Elements")
 
 
 
-
-
-
+structure = Stack()
+structure.pop()
 first_node = Node("Primero en Entrar")
+structure.push(first_node)
 second_node = Node("Segundo en Entrar")
-third_node = Node("Tercero en Entrar")
-structure = Stack(first_node)
-
 structure.push(second_node)
-
-structure.print_structure()
-
+third_node = Node("Tercero en Entrar")
 structure.push(third_node)
-
-structure.print_structure()
-
-structure.pop()
-
 structure.print_structure()
 structure.pop()
-
 structure.print_structure()
-structure.pop()
-
-structure.print_structure()
-structure.pop()
-structure.pop()
-structure.pop()
--- Semana14/exercise_02_double_ended_queue.py
+++ Semana14/exercise_02_double_ended_queue.py
@@ -11,97 +11,96 @@ class Node:
 
 class DoubleEndedQueue:
     head: Node
+    tail: Node
 
-    def __init__(self, head):
-        self.head = head
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
 
     def print_structure(self):
-        current_node = self.head
+        if self.head:
+            current_node = self.head
 
-        while(current_node is not None):
-            print(current_node.data)
-            current_node = current_node.next_right
+            while current_node is not None:
+                print(current_node.data)
+                current_node = current_node.next_right
             
     
     def pop_left(self):
-        if self.head:
+        if self.head is None:
+            return None
+        elif self.head == self.tail:
+            old_head = self.head
+            self.head = None
+            self.tail = None
+            return old_head.data
+        else:
+            old_head = self.head
             self.head = self.head.next_right
-            
-    
+            self.head.next_left = None
+            return old_head.data
+
     def pop_right(self):
-        if self.head:
-            current_node = self.head
-            last_node = Node
-
-            while current_node.next_right is not None:
-                last_node = current_node
-                current_node = current_node.next_right
-            
-            last_node.next_right = None
-            current_node.next_left = None
-
-
-    def push_right(self, new_node):
-        current_node = self.head
-
-        while current_node.next_right is not None:
-            current_node = current_node.next_right
-        
-        current_node.next_right = new_node
-        new_node.next_left = current_node
-        new_node.next_right = None
+        if self.tail is None:
+            return None
+        elif self.head == self.tail:
+            old_tail = self.tail
+            self.head = None
+            self.tail = None
+            return old_tail.data
+        else:
+            old_tail = self.tail
+            self.tail = self.tail.next_left
+            self.tail.next_right = None
+            return old_tail.data
     
-
-    def push_left(self, new_node):
-        if self.head:
+    def push_right(self, new_node):
+        if self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+            new_node.next_right = None
+            new_node.next_left = None
+        else:
+            self.tail.next_right = new_node
+            new_node.next_left = self.tail
+            new_node.next_right = None
+            self.tail = new_node
+            
+    def push_left(self,new_node):
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            new_node.next_left = None
+            new_node.next_right = None
+        else:
+            self.head.next_left = new_node
             new_node.next_right = self.head
             new_node.next_left = None
             self.head = new_node
+            
 
-'''
-print("Insert Head")
-first_node = Node("Soy el primero")
-print("Insert Second Node")
-second_node = Node("Soy el segundo")
-structure = DoubleEndedQueue(first_node)
-structure.push_right(second_node)
-print("Insert Third Node at the End")
-third_node = Node("Soy el tercero")
+
+
+structure = DoubleEndedQueue()
+first_node = Node("First")
+structure.push_left(first_node)
+second_node = Node("Second")
+structure.push_left(second_node)
+third_node = Node("Third")
 structure.push_right(third_node)
-print("Print 1 - 2 - 3")
-structure.print_structure()
-print("Insert New Head")
-forth_node = Node("Soy el cuarto")
-structure.push_left(forth_node)
-print("Print 4- 1 - 2 - 3 with a New Head")
-structure.print_structure()
-fifth_node = Node("Soy el Quinto")
-print("Insert New Head")
+forth_node = Node("Forth")
+structure.push_right(forth_node)
+fifth_node = Node("Fifth")
 structure.push_left(fifth_node)
-print("Print 5 - 4- 1 - 2 - 3 with a New Head")
-structure.print_structure()
-print("Pop head")
 structure.pop_left()
-print("Print 4- 1 - 2 - 3 with a New Head")
-structure.print_structure()
-print("Pop Right")
-structure.pop_right()
-print("Print 4- 1 - 2")
-structure.print_structure()
-six_node = Node("Soy el Sexto")
-structure.push_right(six_node)
-print("Print 4- 1 - 2 - 6")
-structure.print_structure()
 structure.pop_right()
 structure.pop_right()
 structure.pop_right()
 structure.pop_right()
-structure.pop_right()
-
-structure.print_structure()
 structure.pop_left()
 
+print("structure.print")
 structure.print_structure()
-structure.pop_right()
-structure.pop_left()
-'''
+
+
